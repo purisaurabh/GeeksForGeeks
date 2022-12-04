3748024758ec60efe87c0ee06cdfb24619134e77
Saurabh Puri <87465792+purisaurabh@users.noreply.github.com>
2022-12-04 13:20:56 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Maximize Number of 1's/maximize-number-of-1s.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+public:
+    // m is maximum of number zeroes allowed to flip
+    // n is size of array
+    int findZeroes(int arr[], int n, int m) 
+    {
+        // code here
+        int i=0;
+       int j=0;
+       
+       while(i<n)
+       {
+           if(arr[i]==0)
+               m--;
+           if(m<0)
+           {
+               if(arr[j]==0)
+               {
+                   m++;
+               }
+               j++;
+           }
+           i++;
+       }
+       return i-j;
+    }  
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i, m;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        cin >> m;
+        Solution ob;
+        auto ans = ob.findZeroes(arr, n, m);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
