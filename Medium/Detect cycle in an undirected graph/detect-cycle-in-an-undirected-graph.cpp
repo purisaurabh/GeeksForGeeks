d11b425eefa1e856f72b2ad42bef91fb08861506
Saurabh Puri <87465792+purisaurabh@users.noreply.github.com>
2022-12-29 12:12:49 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Detect cycle in an undirected graph/detect-cycle-in-an-undirected-graph.cpp
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    
+    bool detect(int source , vector<int>adj[] , int visited[])
+    {
+        visited[source] = 1;
+        
+        queue<pair<int,int>>q;
+        
+        // firrt node which is came from -1
+        q.push({source , -1});
+        
+        while(!q.empty())
+        {
+            int node = q.front().first;
+            int parent = q.front().second;
+            q.pop();
+            
+            for(auto adjacentNode : adj[node])
+            {
+                if(!visited[adjacentNode])
+                {
+                    visited[adjacentNode] = 1;
+                    q.push({adjacentNode , node});
+                }
+                
+                else if(parent != adjacentNode)
+                {
+                    return true;
+                }
+            }
+            
+        }
+        
+        
+        return false;
+    }
+  
+    // Function to detect cycle in an undirected graph.
+    bool isCycle(int V, vector<int> adj[]) 
+    {
+        // Code here
+        int visited[V] = {0};
+        for(int i = 0; i < V ; i++)
+        {
+            if(!visited[i])
+            {
+                if(detect(i , adj , visited))
+                    return true;
+            }
+        }
+        
+        return false;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        Solution obj;
+        bool ans = obj.isCycle(V, adj);
+        if (ans)
+            cout << "1\n";
+        else
+            cout << "0\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
