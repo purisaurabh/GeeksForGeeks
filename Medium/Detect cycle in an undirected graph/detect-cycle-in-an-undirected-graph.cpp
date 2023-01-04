b26a3f34cd6f76a95fac1486522a01945ffdb36d
Saurabh Puri <87465792+purisaurabh@users.noreply.github.com>
2023-01-04 16:04:28 +0530

    Updated Solution - GfG to GitHub

--- Medium/Detect cycle in an undirected graph/detect-cycle-in-an-undirected-graph.cpp
+++ Medium/Detect cycle in an undirected graph/detect-cycle-in-an-undirected-graph.cpp
@@ -3,62 +3,50 @@
 using namespace std;
 
 // } Driver Code Ends
-class Solution {
-  public:
-    
-    bool detect(int source , vector<int>adj[] , int visited[])
+
+class Solution 
+{
+  private: 
+    bool dfs(int node , int parent , int vis[], vector<int> adj[]) 
     {
-        visited[source] = 1;
-        
-        queue<pair<int,int>>q;
-        
-        // firrt node which is came from -1
-        q.push({source , -1});
-        
-        while(!q.empty())
+        vis[node] = 1; 
+        // visit adjacent nodes
+        for(auto adjacentNode: adj[node]) 
         {
-            int node = q.front().first;
-            int parent = q.front().second;
-            q.pop();
-            
-            for(auto adjacentNode : adj[node])
+            // unvisited adjacent node
+            if(!vis[adjacentNode]) 
             {
-                if(!visited[adjacentNode])
-                {
-                    visited[adjacentNode] = 1;
-                    q.push({adjacentNode , node});
-                }
-                
-                else if(parent != adjacentNode)
-                {
-                    return true;
-                }
+                if(dfs(adjacentNode, node, vis, adj) == true) 
+                    return true; 
             }
             
+            // visited node but not a parent node
+            else if(adjacentNode != parent) 
+                return true; 
         }
-        
-        
-        return false;
+        return false; 
     }
-  
+    
+  public:
     // Function to detect cycle in an undirected graph.
-    bool isCycle(int V, vector<int> adj[]) 
+    bool isCycle(int V, vector<int> adj[])
     {
-        // Code here
-        int visited[V] = {0};
-        for(int i = 0; i < V ; i++)
-        {
-            if(!visited[i])
-            {
-                if(detect(i , adj , visited))
-                    return true;
-            }
-        }
-        
-        return false;
-        
+       int vis[V] = {0}; 
+       
+       // for graph with connected components 
+       for(int i = 0 ; i < V ; i++) 
+       {
+           if(!vis[i]) 
+           {
+               if(dfs(i, -1, vis, adj) == true) 
+                    return true; 
+           }
+       }
+       return false; 
     }
 };
+
+
 
 //{ Driver Code Starts.
 int main() {
