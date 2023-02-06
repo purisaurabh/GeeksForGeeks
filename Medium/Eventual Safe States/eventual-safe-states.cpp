b823da2da4d4f43c045cc8f6f3ef69a7a31ab2a9
Saurabh Puri <87465792+purisaurabh@users.noreply.github.com>
2023-02-06 12:59:18 +0530

    Updated Solution - GfG to GitHub

--- Medium/Eventual Safe States/eventual-safe-states.cpp
+++ Medium/Eventual Safe States/eventual-safe-states.cpp
@@ -10,7 +10,9 @@ using namespace std;
 
 class Solution {
   public:
-  
+    
+    /*
+    // this is the appraoch using dfs 
   
     bool dfs(int node , vector<int>adj[] , int visited[] , int visited_path[] , int check[])
     {
@@ -63,9 +65,63 @@ class Solution {
         
         
         return safe_nodes;
-        
-        
     }
+    */
+    
+    
+    
+    // using thr topollogical sort or bfs
+    
+    vector<int> eventualSafeNodes(int V, vector<int> adj[]) 
+    {
+        // first step is that we have to reverse the edge and vertex means we have to reverse the graph
+        vector<int>adj_rev[V];
+        vector<int>safe_node;
+        
+        int indegree[V] = {0};
+        
+        for(int i = 0 ; i < V ; i++)
+        {
+            for(auto it : adj[i])
+            {
+                adj_rev[it].push_back(i);
+                indegree[i]++;
+            }
+        }
+
+        
+        queue<int>q;
+        for(int i = 0 ; i < V ; i++)
+        {
+            if(indegree[i] == 0)
+            {
+                q.push(i);
+            }
+        }
+        
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            
+            safe_node.push_back(node);
+            
+            for(auto it : adj_rev[node])
+            {
+                indegree[it]--;
+                
+                if(indegree[it] == 0)
+                {
+                    q.push(it);
+                }
+            }
+        }
+        
+        sort(safe_node.begin() , safe_node.end());
+        return safe_node;
+    }
+    
+    
 };
 
 
