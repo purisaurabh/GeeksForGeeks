8ef1c9789d12defc25e6532796cf9710d0a63573
Saurabh Puri <87465792+purisaurabh@users.noreply.github.com>
2023-02-11 11:42:56 +0530

    Updated Solution - GfG to GitHub

--- Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -3,10 +3,15 @@
 using namespace std;
 
 // } Driver Code Ends
+
+// TC = O(E log E)
+// SC = O(E) 
+
 class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+	/*
     int spanningTree(int V, vector<vector<int>> adj[])
     {
         // code here
@@ -53,7 +58,121 @@ class Solution
         
         return sum;
     }
-};
+    */
+    
+    // Kruskal Algorithm
+
+    class Dis_joint_set {
+    
+        vector<int>rank;
+        vector<int>parent;
+        vector<int>size;
+    
+        public:
+        Dis_joint_set(int n)
+        {
+            rank.resize(n+1 , 0);
+            parent.resize(n+1);
+            size.resize(n+1);
+            for(int i = 0 ; i <= n ; i++)
+            {
+                parent[i] = i;
+                size[i] = 1;
+            }
+        }
+    
+        int find_parent(int node)
+        {
+            if(node == parent[node])
+                return node;
+            return parent[node] = find_parent(parent[node]);
+        }
+    
+        void union_by_rank(int u , int v)
+        {
+            int ultimate_u = find_parent(u);
+            int ultimate_v = find_parent(v);
+    
+            if(ultimate_u == ultimate_v)
+                return;
+            
+            if(rank[ultimate_u] < rank[ultimate_v])
+            {
+                parent[ultimate_u] = ultimate_v;
+            }
+    
+            else if(rank[ultimate_v] < rank[ultimate_u])
+            {
+                parent[ultimate_v] = ultimate_u;
+            }
+    
+            else
+            {
+                parent[ultimate_v] = ultimate_u;
+                rank[ultimate_u]++;
+            }
+        }
+    
+    
+        void union_by_size(int u , int v)
+        {
+            int ultimate_u = find_parent(u);
+            int ultimate_v = find_parent(v);
+    
+            if(ultimate_u == ultimate_v)
+                return;
+            
+            if(size[ultimate_u] < size[ultimate_v])
+            {
+                parent[ultimate_u] = ultimate_v;
+                size[ultimate_v] += size[ultimate_u];
+            }
+    
+            else
+            {
+                parent[ultimate_v] = ultimate_u;
+                size[ultimate_u] += size[ultimate_v];
+            }
+        }
+    };
+        
+        // Minimum spanning tree using the kruskal algorithm and disjoint set
+        int spanningTree(int V, vector<vector<int>> adj[])
+        {
+            vector<pair<int,pair<int,int>>> edges;
+            for(int i = 0 ; i < V ; i++)
+            {
+                for(auto it : adj[i])
+                {
+                    int adj_node = it[0];
+                    int wt = it[1];
+                    int node = i;
+                    
+                    edges.push_back({wt , {node , adj_node}});
+                }
+            }
+            
+            Dis_joint_set ds(V);
+            sort(edges.begin() , edges.end());
+            int mst_wt = 0;
+            
+            for(auto it : edges)
+            {
+                int wt = it.first;
+                int u = it.second.first;
+                int v = it.second.second;
+                
+                
+                if(ds.find_parent(u) != ds.find_parent(v))
+                {
+                    mst_wt += wt;
+                    ds.union_by_size(u , v);
+                }
+            }
+            
+            return mst_wt;
+        }
+    };
 
 //{ Driver Code Starts.
 
