c465e7f6cc0d6159755374f5563bd4bdeb9a5602
Saurabh Puri <87465792+purisaurabh@users.noreply.github.com>
2023-04-28 22:25:35 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Geek's Village and Wells/geeks-village-and-wells.cpp
@@ -0,0 +1,110 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+int dx[4]={1,-1,0,0};
+int dy[4]={0,0,1,-1};
+    vector<vector<int>> chefAndWells(int n,int m,vector<vector<char>> &c){
+        // Code here
+        //vector<vector<bool>>vis(n,vector<bool>(m,false));
+        vector<vector<int>>ans(n,vector<int>(m,-1));
+        queue<pair<pair<int,int>,int>>q;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(c[i][j]=='N' )
+                {
+                    ans[i][j]=0;
+                    
+                }
+                if(c[i][j]=='W')
+                {
+                    q.push({{i,j},0});
+                    ans[i][j]=0;
+                    
+                }
+                if(c[i][j]=='.')
+                {
+                    ans[i][j]=-10;
+                }
+            }
+        }
+        
+        while(!q.empty())
+        {
+            auto top = q.front();
+            q.pop();
+            for(int i=0;i<4;i++)
+            {
+                int newx = top.first.first + dx[i];
+                int newy = top.first.second + dy[i];
+                int dist = top.second;
+               
+                if(newx>=0&&newy>=0&&newx<n&&newy<m&&c[newx][newy]!=0)
+                {
+                    int canbe = dist+2;
+                    if(ans[newx][newy] > canbe || ans[newx][newy]==-1)
+                    {
+                        ans[newx][newy]=canbe;
+                        q.push({{newx,newy},canbe});
+                    }
+                    else if(ans[newx][newy] == -10)
+                    {
+                        ans[newx][newy]=0;
+                        q.push({{newx,newy},canbe});
+                    }
+                    
+                }
+            }
+            
+        }
+         for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(ans[i][j]==-10)
+                {
+                    ans[i][j]=0;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,m;
+        cin>>n>>m;
+        vector<vector<char>> c(n,vector<char>(m));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                cin>>c[i][j];
+            }
+        }
+        Solution ob;
+        vector<vector<int>> res=ob.chefAndWells(n,m,c);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                cout<<res[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+}
+// } Driver Code Ends
