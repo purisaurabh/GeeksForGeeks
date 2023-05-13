19920eaf9dd1afe948710735cc0e834ac6b69945
Saurabh Puri <87465792+purisaurabh@users.noreply.github.com>
2023-05-13 20:38:46 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Bit Magic/bit-magic.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int bitMagic(int n, vector<int> &arr) {
+        // code here
+        
+        int l =0 , r =n-1 ,ans =0 ;
+        while(l<=r)
+        {
+            if(arr[l++]!=arr[r--])
+            ans++; 
+        }
+        if(ans%2==0)
+        return ans/2 ;
+        return ans/2 + 1 ;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int n;
+        scanf("%d",&n);
+        
+        
+        vector<int> arr(n);
+        Array::input(arr,n);
+        
+        Solution obj;
+        int res = obj.bitMagic(n, arr);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
