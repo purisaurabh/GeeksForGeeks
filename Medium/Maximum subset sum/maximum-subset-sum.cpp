57ed87c0239a94d6c7710eb4b80d5615ce4edeff
Saurabh Puri <87465792+purisaurabh@users.noreply.github.com>
2023-05-14 08:34:57 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Maximum subset sum/maximum-subset-sum.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class IntArray
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+
+  public:
+    long long findMaxSubsetSum(int N, vector<int> A) {
+        A.push_back(0);
+        int p1 = 0;
+        int p2 = 0;
+        int c = 0;
+        for(int i=0; i<=N; i++){
+            c = max(p1+A[i], p2+A[i]);
+            p2 = p1;
+            p1 = c;
+        }
+        return p1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N;
+        scanf("%d",&N);
+        
+        
+        vector<int> A(N);
+        IntArray::input(A,N);
+        
+        Solution obj;
+        long long res = obj.findMaxSubsetSum(N, A);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
