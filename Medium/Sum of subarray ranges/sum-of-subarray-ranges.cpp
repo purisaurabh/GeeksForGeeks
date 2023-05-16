407fedfd9408c4ff36a949ec53235a09b151f6fa
Saurabh Puri <87465792+purisaurabh@users.noreply.github.com>
2023-05-16 23:03:23 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Sum of subarray ranges/sum-of-subarray-ranges.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    long long subarrayRanges(int n, vector<int> &arr) 
+    {
+        long long sum = 0;
+        for(int i = 0; i < n ; i++)
+        {
+            int mini = arr[i];
+            int maxi = arr[i];
+            
+            for(int j = i ; j < n ; j++)
+            {
+                mini = min(mini , arr[j]);
+                maxi = max(maxi , arr[j]);
+                
+                sum += maxi - mini;
+            }
+        }
+        
+        return sum;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+
+        vector<int> arr(N);
+        for (int i = 0; i < N; i++) cin >> arr[i];
+
+        Solution obj;
+        cout << obj.subarrayRanges(N, arr) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
